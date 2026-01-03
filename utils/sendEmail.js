ade7337362e9424256c1540fd6d075086ee45767
DivyaBitmax <divyasinghbitmax.com>
2026-01-03 11:45:55 +0530

    changes in mailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,39 @@
+// import nodemailer from "nodemailer";
+// import config from "../config/config.js";
+
+// const transporter = nodemailer.createTransport({
+//   host: config.smtpHost,
+//   port: config.smtpPort,
+//   secure: true, // 465 ke liye true
+//   auth: {
+//     user: config.emailUser,
+//     pass: config.emailPass,
+//   },
+// });
+
+// export const sendEmail = async ({ to, subject, html }) => {
+//   return transporter.sendMail({
+//     from: `"Bitmax Franchise" <${config.emailUser}>`,
+//     to,
+//     subject,
+//     html,
+//   });
+// };
+
+
 import nodemailer from "nodemailer";
 import config from "../config/config.js";
 
 const transporter = nodemailer.createTransport({
   host: config.smtpHost,
-  port: config.smtpPort,
-  secure: true, // 465 ke liye true
+  port: Number(config.smtpPort), // IMPORTANT
+  secure: false, // ❌ 465 ke liye hota hai | 587 ke liye false
   auth: {
     user: config.emailUser,
     pass: config.emailPass,
+  },
+  tls: {
+    rejectUnauthorized: false, // hosting SMTP ke liye common
   },
 });
 
